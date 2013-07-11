0e4d45d4be364b315cee0beeda8f2c4aa3b6583f
jamesmichiemo <james.michiemo@gmail.com>
2013-07-11 00:33:09 +0000

    use previous code with fs.readFileSync function to print out the index.html as a string

--- web.js
+++ web.js
@@ -1,1 +1,14 @@
-fs.readFileSync("index.html","utf8"); // => "Hello World from index.html"
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var content = fs.readFileSync("index.html","utf8"); // <= "Hello World from index.html"
+
+app.get('/', function(request, response) {
+  response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
