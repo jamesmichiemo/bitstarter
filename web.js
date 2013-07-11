e9eac19205301ac3d6bae2bdd714940b4360fb3d
jamesmichiemo <james.michiemo@gmail.com>
2013-07-11 00:56:24 +0000

    Test previous code output with hard coded string

--- web.js
+++ web.js
@@ -1,3 +1,14 @@
-var content = fs.readFileSync('index.html','utf8');
+var express = require('express');
 
-console.log(content);
+var content = fs.readFileSync('index.html','utf8');
+ 
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World from index.html');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
