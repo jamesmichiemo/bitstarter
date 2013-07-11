aeb1338409ff55b1b0129d7e0627f8cfa5de615c
jamesmichiemo <james.michiemo@gmail.com>
2013-07-11 02:12:21 +0000

    add details to comments

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express'); // http://expressjs.com/api.html
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
+var fs = require('fs'); // fs cannot be used without being referenced https://github.com/mhevery/jasmine-node/issues/186
 
 app.get('/', function(request, response) {
   var content = fs.readFileSync("index.html","utf8");
