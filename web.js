f85bf3160aa1f4106a3ef0dd2d00cba33e4014b6
jamesmichiemo <james.michiemo@gmail.com>
2013-07-11 00:46:50 +0000

    print content from index.html using console.log

--- web.js
+++ web.js
@@ -1,14 +1,3 @@
-var express = require('express');
+var content = fs.readFileSync('index.html','utf8');
 
-var app = express.createServer(express.logger());
-
-var content = fs.readFileSync("index.html","utf8"); // <= "Hello World from index.html"
-
-app.get('/', function(request, response) {
-  response.send(content);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+console.log(content);
